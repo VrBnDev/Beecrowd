c588508f6b7f14eb3a0441e0f3db306797204d25
VrBnDev <rafaelavborgess@gmail.com>
2025-10-03 17:41:30 -0300

    :white_check_mark: Resposta aceita

--- bee1022 - TDA Racional.py
+++ bee1022 - TDA Racional.py
@@ -11,15 +11,28 @@ for i in range(entrada):
         parte_soma_2 = (d1*d2)
         result_soma = str(parte_soma_1) + '/' + str(parte_soma_2)
         
-        print(f'{result_soma}')
+        print(f'{result_soma} = {int(parte_soma_1/gcd(parte_soma_1,parte_soma_2))}/{int(parte_soma_2/gcd(parte_soma_1,parte_soma_2))}')
+    
     # Subtração
     elif ent[3] == '-':
-        result_subtracao = (n1*d2-n2*d1)/(d1*d2)
+        parte_subtracao_1 = (n1*d2-n2*d1)
+        parte_subtracao_2 = (d1*d2)
+        result_subtracao = str(parte_subtracao_1) + '/' + str(parte_subtracao_2)
+    
+        print(f'{result_subtracao} = {int(parte_subtracao_1/gcd(parte_subtracao_1,parte_subtracao_2))}/{int(parte_subtracao_2/gcd(parte_subtracao_1,parte_subtracao_2))}')
+    
     # Multiplicação
     elif ent[3] == '*':
-        result_multiplicacao = (n1*n2)/(d1*d2)
+        parte_multiplicacao_1 = (n1*n2)
+        parte_multiplicacao_2 = (d1*d2)
+        result_multiplicacao = str(parte_multiplicacao_1) + '/' + str(parte_multiplicacao_2)
+    
+        print(f'{result_multiplicacao} = {int(parte_multiplicacao_1/gcd(parte_multiplicacao_1,parte_multiplicacao_2))}/{int(parte_multiplicacao_2/gcd(parte_multiplicacao_1,parte_multiplicacao_2))}')
     # Divisão
     else:
-        result_divisao = (n1*d2)/(n2*d1)
-
+        parte_divisao_1 = (n1*d2)
+        parte_divisao_2 = (n2*d1)
+        result_divisao = str(parte_divisao_1) + '/' + str(parte_divisao_2)
+    
+        print(f'{result_divisao} = {int(parte_divisao_1/gcd(parte_divisao_1,parte_divisao_2))}/{int(parte_divisao_2/gcd(parte_divisao_1,parte_divisao_2))}')
 
